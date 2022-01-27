d14db5921b383481d008352bf617e21b0715b42f
VadimPonomarov <pvs.versia@gmail.com>
2022-01-27 20:30:15 +0200

    Lesson_4_HW

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
 import {React, useReducer} from "react";
 
-import Counter from './components/counter/Counter'
-
-const INC = 'inc'
-const DEC = 'dec'
-const RES = 'res'
+import counterGenerator from './servises/counter_generator.service'
 
 const reducer = (state, action) => {
-    console.log(state)
+    const CURRENT = `counter${action.payload.index}`
+
     switch (action.type) {
         case 'inc':
-            return {...state, [action.payload.hhh]: {name: ',jhv'}}
+            return {...state, [CURRENT]: state[CURRENT] + 1}
         case "dec":
+            return {...state, [CURRENT]: state[CURRENT] - 1}
         case "res":
+            return {...state, [CURRENT]: 0}
+        default:
+            return new Error('Error')
     }
 }
 
-
 function App() {
-    const [state, dispatch] = useReducer(reducer, {})
+
+    const initialState = {}
+    const [state, dispatch] = useReducer(reducer, initialState)
+    //Генератор каунтеров
+    const counterArr = counterGenerator(3, initialState, state, dispatch)
+
 
     return (
-        <Counter number={3}/>
+        counterArr.map((item, index) => {
+            return (
+                <div key={index}>
+                    {item}
+                </div>
+            )
+        })
+
     )
 }
 
